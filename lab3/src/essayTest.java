ba03d6e4f9d7701b59e842c955e95b75a1028e16
Phạm Nguyễn Bảo Trang <167573896+TinoDragna@users.noreply.github.com>
2024-08-01 11:20:54 +0700

    1/8/2024 11:21

--- lab3/src/essayTest.java
+++
@@ -1,8 +0,0 @@
-public class essayTest {
-    public static void main(String[] args) {
-        Essay essay = new Essay(30, 20, 20, 30);
-        System.out.println(essay);
-        System.out.println("total = "+essay.total(30, 20, 20, 30));
-    }
-
-}
